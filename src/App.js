d8299b1d040f0a57cf0a57626c7d51041d8d2797
mikegv <xmik3yvx@live.com>
2022-05-27 11:10:33 -0700

    no board

--- src/App.js
+++ src/App.js
@@ -336,7 +336,7 @@ function App() {
     
       {/* <Modal gameOver={gameOver} modalClickHandler={modalClickHandler} board={board} /> */}
       <p style={player1 ? { color: 'black' } : { color: 'rgb(21, 255, 28)' }} >Player 2</p>
-      <Board theme={theme} numberOfStonesInMove={numberOfStonesInMove} clickHandler={clickHandler} board={board} gameOver={gameOver} afterAnimation={afterAnimation} isMoving={isMoving} stonesAnimationPosition={stonesAnimationPosition} topOfBoard={topOfBoard} /> 
+      {/* <Board theme={theme} numberOfStonesInMove={numberOfStonesInMove} clickHandler={clickHandler} board={board} gameOver={gameOver} afterAnimation={afterAnimation} isMoving={isMoving} stonesAnimationPosition={stonesAnimationPosition} topOfBoard={topOfBoard} />  */}
        <p style={!player1 ? { color: 'black' } : { color: 'rgb(21, 255, 28)' }}>Player 1</p> 
     </div>
  
